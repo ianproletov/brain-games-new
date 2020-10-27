c36e632959c5addb497f49e2f466f6a0775155e1
ianproletov <yan.proletov@gmail.com>
2020-10-27 12:00:03 +0300

    add brain-prime

---
+++ src/games/prime.js
@@ -0,0 +1,21 @@
+import makeGame from '..';
+import getRandomNumber from '../utils';
+
+const task = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (number) => {
+  for (let i = 2; i <= number / 2; i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const makeStep = () => {
+  const currentNumber = getRandomNumber(3, 99);
+  const rightAnswer = isPrime(currentNumber) ? 'yes' : 'no';
+  return ({ rightAnswer, question: `${currentNumber}` });
+};
+
+export default () => makeGame(makeStep, task);
